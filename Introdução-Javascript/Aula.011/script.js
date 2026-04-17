ea70f8cd98bfa75f75c4ceed2298b2f452d722af
helenamamede05-prog <helena.mamede05@gmail.com>
2026-04-16 21:15:53 -0300

    Add files via upload

--- Introdução-Javascript/Aula.011/script.js
+++ Introdução-Javascript/Aula.011/script.js
@@ -1,10 +1,12 @@
 let primeiroNumero = document.querySelector("#primeiroNumero")
 let segundoNumero = document.querySelector("#segundoNumero")
 let btnSoma = document.querySelector("#btnSoma")
+let apresentaResultado = document.querySelector("#resultado")
 
 function soma() {
     let resultado = Number(primeiroNumero.value) + Number(segundoNumero.value)
-    alert(resultado)
+    
+    apresentaResultado.innerHTML = 'Resultado: ${resultado}'
 }
 
 btnSoma.addEventListener("click", soma)
---
+++ Introdução-Javascript/Aula.013/script.js
@@ -0,0 +1,3 @@
+let dinheiro = 5000
+
+alert(dinheiro.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'}))
